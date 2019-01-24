f6cf32c23790a5b16731c5dc94aae8080c6e309e
Thomas Watson <w@tson.dk>
2019-01-24 13:08:22 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Elastic APM Node.js OpenTracing Bridge
+
+An [OpenTracing](https://opentracing.io/) bridge for the [Elastic APM Node.js Agent](https://github.com/elastic/apm-agent-nodejs).
+
+[![npm](https://img.shields.io/npm/v/elastic-apm-node-opentracing.svg)](https://www.npmjs.com/package/elastic-apm-node-opentracing)
+[![build status](https://travis-ci.org/elastic/apm-agent-nodejs-opentracing.svg?branch=master)](https://travis-ci.org/elastic/apm-agent-nodejs-opentracing)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+
+## Prerequisites
+
+This module have [`elastic-apm-node`](https://www.npmjs.com/package/elastic-apm-node) as a peer dependency.
+
+## Installation
+
+```
+npm install elastic-apm-node-opentracing --save
+```
+
+## Usage
+
+```js
+// Start the Elastic APM agent at the VERY top of the first file loaded
+// in your app
+const agent = require('elastic-apm-node').start()
+const Tracer = require('elastic-apm-node-opentracing')
+
+// Pass the Elastic APM agent as an argument to the OpenTracing tracer
+const tracer = new Tracer(agent)
+
+const span = tracer.startSpan('my-first-span')
+// ... do some work ...
+span.finish()
+```
+
+## API
+
+### `tracer = new Tracer(agent)`
+
+This modules exposes a `Tracer` class which is OpenTracing compatible.
+
+When instantiating the `Tracer` object,
+an instance of the Elastic APM Node.js Agent must be provided as its only argument.
+
+For details about the `tracer` API,
+see the [opentracing-javescript](https://github.com/opentracing/opentracing-javascript) API docs.
+
+## License
+
+[Apache-2.0](LICENSE)
---
+++ test/tracer.js
@@ -0,0 +1,307 @@
+'use strict'
+
+const opentracing = require('opentracing')
+const test = require('tape')
+
+const { setup, getAgent } = require('./utils')
+const Tracer = require('..')
+
+test('should throw if not given an agent', setup(function (t) {
+  t.throws(function () {
+    new Tracer() // eslint-disable-line no-new
+  })
+  t.end()
+}))
+
+test('should use Agent instance given as argument', setup(function (t) {
+  const agent = getAgent()
+  const tracer = new Tracer(agent)
+
+  t.equal(tracer._agent, agent)
+  t.end()
+}))
+
+test('#startSpan()', setup(function (t) {
+  const tracer = new Tracer(getAgent())
+  const span1 = tracer.startSpan()
+
+  t.ok(span1, 'should return span')
+  t.ok(span1._span, 'should hold reference to span/transaction')
+  t.equal(span1._span.name, 'unnamed', 'should fall back to default name')
+  t.equal(span1._span.type, 'custom', 'should fall back to default type')
+
+  const span2 = tracer.startSpan()
+
+  t.equal(span1._isTransaction, true, 'first span should be a transaction')
+  t.equal(span2._isTransaction, false, 'second span should not be a transaction')
+
+  t.end()
+}))
+
+test('#startSpan(name)', setup(function (t) {
+  const tracer = new Tracer(getAgent())
+  const span = tracer.startSpan('foo')
+
+  t.equal(span._span.name, 'foo', 'should use given name')
+  t.equal(span._span.type, 'custom', 'should fall back to default type')
+  t.end()
+}))
+
+test('#startSpan(name, {tags: {}})', setup(function (t) {
+  const tracer = new Tracer(getAgent())
+  const span = tracer.startSpan('foo', { tags: {} })
+
+  t.equal(span._span.name, 'foo', 'should use given name')
+  t.equal(span._span.type, 'custom', 'should fall back to default type')
+  t.end()
+}))
+
+test('#startSpan(name, {tags: {type}})', setup(function (t) {
+  const tracer = new Tracer(getAgent())
+  const span = tracer.startSpan('foo', { tags: { type: 'bar' } })
+
+  t.equal(span._span.name, 'foo', 'should use given name')
+  t.equal(span._span.type, 'bar', 'should use given type')
+  t.end()
+}))
+
+test('#startSpan(name, {tags: {...}})', setup(function (t) {
+  const opts = { tags: {
+    type: 'bar',
+    a: '1',
+    'a.b': '2',
+    'a"b': '3',
+    'a*b': '4'
+  } }
+
+  const tracer = new Tracer(getAgent())
+  const span = tracer.startSpan('foo', opts)
+
+  t.equal(span._span.name, 'foo', 'should use given name')
+  t.equal(span._span.type, 'bar', 'should use given type')
+
+  t.deepEqual(span._span._tags, {
+    a: '1',
+    a_b: '4'
+  }, 'should set expected tags')
+
+  t.deepEqual(opts, { tags: {
+    type: 'bar',
+    a: '1',
+    'a.b': '2',
+    'a"b': '3',
+    'a*b': '4'
+  } }, 'should not mutate input')
+
+  t.end()
+}))
+
+test('#startSpan(name, {startTime})', setup(function (t) {
+  const startTime = Date.now() - 1000
+
+  const tracer = new Tracer(getAgent())
+  const span = tracer.startSpan('foo', { startTime })
+
+  t.equal(span._span.timestamp, startTime * 1000)
+  t.end()
+}))
+
+test('#startSpan() - implicit children', setup(function (t) {
+  const tracer = new Tracer(getAgent())
+  const span1 = tracer.startSpan('foo')
+  const span2 = tracer.startSpan('bar') // implicit child of span1, because span1 is a transaction
+  const span3 = tracer.startSpan('baz') // implicit child of span1, because span1 is a transaction
+
+  t.equal(span1._isTransaction, true)
+  t.equal(span2._isTransaction, false)
+  t.equal(span3._isTransaction, false)
+
+  const span1Context = span1.context()._context
+  const span2Context = span2.context()._context
+  const span3Context = span3.context()._context
+
+  t.equal(span1Context.version, '00')
+  t.ok(typeof span1Context.traceId, 'string')
+  t.ok(typeof span1Context.id, 'string')
+  t.equal(span1Context.flags, '01')
+
+  // span1 should be the root
+  t.equal(span1Context.parentId, undefined)
+
+  // span2 should be a child of span1
+  t.equal(span2Context.version, span1Context.version)
+  t.equal(span2Context.traceId, span1Context.traceId)
+  t.notEqual(span2Context.id, span1Context.id)
+  t.equal(span2Context.parentId, span1Context.id)
+  t.equal(span2Context.flags, span1Context.flags)
+
+  // span3 should be a child of span1
+  t.equal(span3Context.version, span1Context.version)
+  t.equal(span3Context.traceId, span1Context.traceId)
+  t.notEqual(span3Context.id, span1Context.id)
+  t.equal(span3Context.parentId, span1Context.id)
+  t.equal(span3Context.flags, span1Context.flags)
+
+  t.end()
+}))
+
+test('#startSpan(name, {childOf})', setup(function (t) {
+  const tracer = new Tracer(getAgent())
+  const span1 = tracer.startSpan('foo')
+  const span2 = tracer.startSpan('bar') // implicit child of span1, because span1 is a transaction
+  const span3 = tracer.startSpan('baz', { childOf: span2.context() })
+
+  t.equal(span1._isTransaction, true)
+  t.equal(span2._isTransaction, false)
+  t.equal(span3._isTransaction, false)
+
+  const span1Context = span1.context()._context
+  const span2Context = span2.context()._context
+  const span3Context = span3.context()._context
+
+  t.equal(span1Context.version, '00')
+  t.ok(typeof span1Context.traceId, 'string')
+  t.ok(typeof span1Context.id, 'string')
+  t.equal(span1Context.flags, '01')
+
+  // span1 should be the root
+  t.equal(span1Context.parentId, undefined)
+
+  // span2 should be a child of span1
+  t.equal(span2Context.version, span1Context.version)
+  t.equal(span2Context.traceId, span1Context.traceId)
+  t.notEqual(span2Context.id, span1Context.id)
+  t.equal(span2Context.parentId, span1Context.id)
+  t.equal(span2Context.flags, span1Context.flags)
+
+  // span3 should be a child of span2
+  t.equal(span3Context.version, span2Context.version)
+  t.equal(span3Context.traceId, span2Context.traceId)
+  t.notEqual(span3Context.id, span2Context.id)
+  t.equal(span3Context.parentId, span2Context.id)
+  t.equal(span3Context.flags, span2Context.flags)
+
+  t.end()
+}))
+
+test('#startSpan(name, {references: [childOf]})', setup(function (t) {
+  const tracer = new Tracer(getAgent())
+  const span1 = tracer.startSpan('foo')
+  const span2 = tracer.startSpan('bar') // implicit child of span1, because span1 is a transaction
+  const references = [
+    new opentracing.Reference(opentracing.REFERENCE_CHILD_OF, span2.context())
+  ]
+  const span3 = tracer.startSpan('baz', { references })
+
+  t.equal(span1._isTransaction, true)
+  t.equal(span2._isTransaction, false)
+  t.equal(span3._isTransaction, false)
+
+  const span1Context = span1.context()._context
+  const span2Context = span2.context()._context
+  const span3Context = span3.context()._context
+
+  t.equal(span1Context.version, '00')
+  t.ok(typeof span1Context.traceId, 'string')
+  t.ok(typeof span1Context.id, 'string')
+  t.equal(span1Context.flags, '01')
+
+  // span1 should be the root
+  t.equal(span1Context.parentId, undefined)
+
+  // span2 should be a child of span1
+  t.equal(span2Context.version, span1Context.version)
+  t.equal(span2Context.traceId, span1Context.traceId)
+  t.notEqual(span2Context.id, span1Context.id)
+  t.equal(span2Context.parentId, span1Context.id)
+  t.equal(span2Context.flags, span1Context.flags)
+
+  // span3 should be a child of span2
+  t.equal(span3Context.version, span2Context.version)
+  t.equal(span3Context.traceId, span2Context.traceId)
+  t.notEqual(span3Context.id, span2Context.id)
+  t.equal(span3Context.parentId, span2Context.id)
+  t.equal(span3Context.flags, span2Context.flags)
+
+  t.end()
+}))
+
+// This currently doesn't behave in a logical way as when the followsFrom
+// reference is being ignored, the agent will simply automatically associate
+// span2 with span1, because span1 is a transaction
+test.skip('#startSpan(name, {references: [followsFrom]})', setup(function (t) {
+  const tracer = new Tracer(getAgent())
+  const span1 = tracer.startSpan('foo')
+  const references = [
+    new opentracing.Reference(opentracing.REFERENCE_FOLLOWS_FROM, span1.context())
+  ]
+  const span2 = tracer.startSpan('bar', { references })
+
+  const span1Context = span1.context()._context
+  const span2Context = span2.context()._context
+
+  t.notEqual(span1Context.traceId, span1Context.traceId)
+  t.notEqual(span1Context.id, span1Context.id)
+  t.equal(span2Context.parentId, undefined)
+  t.equal(span1Context.parentId, undefined)
+  t.end()
+}))
+
+test('#startSpan(name, {references: [followsFrom, childOf, childOf]})', setup(function (t) {
+  const tracer = new Tracer(getAgent())
+  const span1 = tracer.startSpan('foo')
+  const span2 = tracer.startSpan('bar') // implicit child of span1, because span1 is a transaction
+  const references = [
+    new opentracing.Reference(opentracing.REFERENCE_FOLLOWS_FROM, span1.context()),
+    new opentracing.Reference(opentracing.REFERENCE_CHILD_OF, span2.context()),
+    new opentracing.Reference(opentracing.REFERENCE_CHILD_OF, span1.context())
+  ]
+  const span3 = tracer.startSpan('baz', { references })
+
+  t.equal(span1._isTransaction, true)
+  t.equal(span2._isTransaction, false)
+  t.equal(span3._isTransaction, false)
+
+  const span1Context = span1.context()._context
+  const span2Context = span2.context()._context
+  const span3Context = span3.context()._context
+
+  t.equal(span1Context.version, '00')
+  t.ok(typeof span1Context.traceId, 'string')
+  t.ok(typeof span1Context.id, 'string')
+  t.equal(span1Context.flags, '01')
+
+  // span1 should be the root
+  t.equal(span1Context.parentId, undefined)
+
+  // span2 should be a child of span1
+  t.equal(span2Context.version, span1Context.version)
+  t.equal(span2Context.traceId, span1Context.traceId)
+  t.notEqual(span2Context.id, span1Context.id)
+  t.equal(span2Context.parentId, span1Context.id)
+  t.equal(span2Context.flags, span1Context.flags)
+
+  // span3 should be a child of span2
+  t.equal(span3Context.version, span2Context.version)
+  t.equal(span3Context.traceId, span2Context.traceId)
+  t.notEqual(span3Context.id, span2Context.id)
+  t.equal(span3Context.parentId, span2Context.id)
+  t.equal(span3Context.flags, span2Context.flags)
+
+  t.end()
+}))
+
+test('#startSpan() - ended transaction', setup(function (t) {
+  const tracer = new Tracer(getAgent())
+
+  const span1 = tracer.startSpan('foo')
+  span1.finish()
+
+  const span2 = tracer.startSpan('bar')
+
+  t.equal(span1._isTransaction, true)
+  t.equal(span2._isTransaction, true)
+  t.equal(span1.context()._context.parentId, undefined)
+  t.equal(span2.context()._context.parentId, undefined)
+  t.end()
+}))
